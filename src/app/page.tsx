185205886a33c5c02ff4da2eba370c698384390c
Martin D'Uva <martinduva1998@hotmail.com>
2025-02-03 12:57:54 -0300

    feat: modal close icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <div className="grid min-h-screen grid-cols-4 place-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="col-span-full grid grid-cols-subgrid gap-y-10">
-        <h1 className="col-span-full text-center text-2xl sm:text-left sm:text-3xl">
+        <h1 className="col-span-full text-center text-2xl md:text-left md:text-3xl">
           Patient Data Management
         </h1>
         <Suspense fallback={<PatientsListSkeleton amount={50} />}>
--- src/components/ui/Dialog/index.tsx
+++ src/components/ui/Dialog/index.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 
 import { cn } from "@/lib/utils";
+import { XIcon } from "../XIcon";
 
 const Dialog = DialogPrimitive.Root;
 
@@ -40,7 +41,8 @@ function DialogContent({ children, className, ...props }: DialogContentProps) {
       >
         {children}
         <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-1 focus:ring-offset-2 disabled:pointer-events-none">
-          X<span className="sr-only">Close</span>
+          <XIcon />
+          <span className="sr-only">Close</span>
         </DialogClose>
       </DialogPrimitive.Content>
     </DialogPrimitive.Portal>
